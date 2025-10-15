214d84d96764e8fd88ebb0866ff13bc58a884393
teyter <teitur93@gmail.com>
2025-10-15 14:10:45 +0000

    fix: larger numbers, atoi >> strtoull

--- codewars/descending_order.c
+++ codewars/descending_order.c
@@ -1,5 +1,5 @@
 // Descending Order
-// https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/c
+// https://www.codewars.com/kata/5467e4d82edf8bbf40000155/c
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,56 +19,36 @@ void print_int_arr(int *arr)
     printf("\n");
 }
 
-int comp(const void* a, const void* b) {
-  	
-  	// If a is smaller, positive value will be returned
+int comp(const void* a, const void* b) 
+{
     return (*(int*)b - *(int*)a);
 }
 
 uint64_t descendingOrder(uint64_t n)
 {
     if (n < 10) return n;
-    char string[256];  
+    char string[32];  
     sprintf(string, "%lu", n);
-    // printf("%s\n", string);
+
     int len = strlen(string);
-
-    // char *sorted = malloc( sizeof(char) * (len + 1) );
     int size = (int)(sizeof(int) * (len));
-
-    //printf("size = %d\n", size);
     int *sort = malloc( size );
-    /**
-    printf("sizeof(int) * (len + 1) = %ld\n", sizeof(int) * (len + 1));
-    printf("sizeof(int): %ld\n", sizeof(int));
-    printf("len: %d\n", len);
-    printf("sorted: %ld\n", sizeof(sorted));
-    printf("len(sortd): %ld\n", sizeof(sorted) / sizeof(sorted[0]));
-     */
-    
     for (int i = 0; i < len; i++)
     {
         sort[i] = (int)string[i];
     }
-    // long siz = sizeof(sorted) / sizeof(sorted[0]);
-    // printf("sizeof(sorted): %ld\nsizeof(sorted[0]): %ld\n", sizeof(sorted), sizeof(sorted[0]));
-    // printf("siz: %ld\n", siz);
-    //print_int_arr(sort);
+
     qsort(sort, size/sizeof(int), sizeof(int), comp);
-    //print_int_arr(sort);
-    
     char *sorted = malloc( sizeof(char) * (len + 1));
+
     for (int i = 0; i < len; i++)
     {
         sorted[i] = (char)sort[i];
-        //printf("(char)sort[i]-48: %c\n", (char)sort[i]);
-        //printf("sort[i]-48: %d\n", sort[i]);
     }
-    //printf("string: %s\n", sorted);
-    uint64_t ret = atoi(sorted);
+    sorted[len] = '\0';
+    uint64_t ret = strtoull(sorted, NULL, 10);
     free(sort);
     free(sorted);
-    printf("return: %ld\n", ret);
     return ret;
 }
 
